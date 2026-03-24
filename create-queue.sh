ac80a816c207d974ff6b569e2ce3e6f59dead2ae
Amruthpn <72870933+Amruthpn@users.noreply.github.com>
2026-03-24 17:11:26 +0530

    Create create-queue.sh

---
+++ create-queue.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+curl -u $SOLACE_USER:$SOLACE_PASS \
+  -X POST "http://$SOLACE_HOST:8080/SEMP/v2/config/msgVpns/kafka-test/queues" \
+  -H "Content-Type: application/json" \
+  -d '{
+        "queueName": "jenkins-queue",
+        "accessType": "exclusive",
+        "permission": "consume",
+        "ingressEnabled": true,
+        "egressEnabled": true
+      }'
